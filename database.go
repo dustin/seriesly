7bb389af0f4d5108e68373489ef7ff5f806bd6d6
Dustin Sallings <dustin@spy.net>
2012-08-27 20:36:39 -0700

    Implemented async writes.
    Flushes every five seconds (not currently configurable).  Doesn't ever
    close write DBs.

--- database.go
+++ database.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/dustin/go-couchstore"
 )
+
+const maxOpQueue = 1000
+
+type dbOperation uint8
+
+const (
+	db_store_item = dbOperation(iota)
+	db_delete_item
+	db_compact
+)
+
+type dbqitem struct {
+	dbname string
+	k      string
+	data   []byte
+	op     dbOperation
+}
+
+type dbWriter struct {
+	ch   chan dbqitem
+	quit chan bool
+	db   *couchstore.Couchstore
+}
+
+var dbLock = sync.Mutex{}
+var dbConns = map[string]*dbWriter{}
 
 func dbPath(n string) string {
 	return filepath.Join(*dbRoot, n) + ".couch"
@@ -43,6 +71,15 @@ func dbcreate(path string) error {
 }
 
 func dbdelete(dbname string) error {
+	dbLock.Lock()
+	defer dbLock.Unlock()
+
+	writer := dbConns[dbname]
+	if writer != nil {
+		writer.quit <- true
+	}
+	delete(dbConns, dbname)
+
 	return os.Remove(dbPath(dbname))
 }
 
@@ -57,16 +94,95 @@ func dblist(root string) []string {
 	return rv
 }
 
-func dbstore(dbname string, k string, body []byte) error {
+func dbWriteLoop(dq *dbWriter) {
+	defer dq.db.Close()
+
+	queued := 0
+	bulk := dq.db.Bulk()
+	defer bulk.Close()
+	defer bulk.Commit()
+
+	flushTime := time.Second * 5
+
+	t := time.NewTimer(flushTime)
+
+	for {
+		select {
+		case <-dq.quit:
+			return
+		case qi := <-dq.ch:
+			switch qi.op {
+			case db_store_item:
+				bulk.Set(couchstore.NewDocInfo(qi.k,
+					couchstore.DocIsCompressed),
+					couchstore.NewDocument(qi.k,
+						string(qi.data)))
+			case db_delete_item:
+				bulk.Delete(couchstore.NewDocInfo(qi.k, 0))
+			default:
+				log.Panicf("Unhandled case: %v", qi.op)
+			}
+			queued++
+			if queued > maxOpQueue {
+				log.Printf("Flushing %d items on queue")
+				bulk.Commit()
+				queued = 0
+				t.Stop()
+				t = time.NewTimer(flushTime)
+			}
+		case <-t.C:
+			if queued > 0 {
+				log.Printf("Flushing %d items on a timer", queued)
+				bulk.Commit()
+				queued = 0
+			}
+			t = time.NewTimer(flushTime)
+		}
+	}
+}
+
+func dbWriteFun(dbname string) (*dbWriter, error) {
 	db, err := dbopen(dbname)
+	if err != nil {
+		return nil, err
+	}
+
+	writer := &dbWriter{
+		make(chan dbqitem),
+		make(chan bool),
+		db,
+	}
+
+	go dbWriteLoop(writer)
+
+	return writer, nil
+}
+
+func getOrCreateDB(dbname string) (*dbWriter, error) {
+	dbLock.Lock()
+	defer dbLock.Unlock()
+
+	writer := dbConns[dbname]
+	var err error
+	if writer == nil {
+		writer, err = dbWriteFun(dbname)
+		if err != nil {
+			return nil, err
+		}
+		dbConns[dbname] = writer
+	}
+	return writer, nil
+}
+
+func dbstore(dbname string, k string, body []byte) error {
+	writer, err := getOrCreateDB(dbname)
 	if err != nil {
 		return err
 	}
-	defer db.Close()
-	defer db.Commit()
 
-	return db.Set(couchstore.NewDocInfo(k, couchstore.DocIsCompressed),
-		couchstore.NewDocument(k, string(body)))
+	writer.ch <- dbqitem{dbname, k, body, db_store_item}
+
+	return nil
 }
 
 func parseKey(s string) int64 {
