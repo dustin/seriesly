b6c504e6f8e8248fbd47d06377c994f51a82d36a
Dustin Sallings <dustin@spy.net>
2012-08-28 18:35:31 -0700

    Added benchmark test for json parsing.

--- query_test.go
+++ query_test.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"reflect"
 	"testing"
 )
 
 var testInput = []*string{}
 
+var bigInput []byte
+
 func init() {
 	testInput = append(testInput, nil)
 	s := []string{"31", "63", "foo"}
 	for i := range s {
 		testInput = append(testInput, &s[i])
+	}
+
+	var err error
+	bigInput, err = ioutil.ReadFile("sample.json")
+	if err != nil {
+		panic("Couldn't read sample.json")
 	}
 }
 
@@ -37,6 +47,17 @@ func TestReducers(t *testing.T) {
 			t.Errorf("Expected %v for %v, got %v",
 				test.exp, test.reducer, got)
 			t.Fail()
+		}
+	}
+}
+
+func BenchmarkJSONParser(b *testing.B) {
+	b.SetBytes(int64(len(bigInput)))
+	for i := 0; i < b.N; i++ {
+		m := map[string]interface{}{}
+		err := json.Unmarshal(bigInput, &m)
+		if err != nil {
+			b.Fatalf("Error unmarshaling json: %v", err)
 		}
 	}
 }
