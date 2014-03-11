6bf6082496fbbdb0178e93709ee041c376e426e9
Dustin Sallings <dustin@spy.net>
2014-03-11 00:08:32 -0700

    Added tests for closeAll

--- util_test.go
+++ util_test.go
@@ -4,6 +4,24 @@ import (
 	"testing"
 )
 
+func TestCloseAllEmpty(t *testing.T) {
+	closeAll([]chan int{})
+}
+
+func TestCloseAllTwo(t *testing.T) {
+	ichan := make(chan int)
+	i2chan := make(chan int)
+	closeAll([]chan int{ichan, i2chan})
+	_, ok := <-ichan
+	if ok {
+		t.Errorf("ichan wasn't closed")
+	}
+	_, ok = <-i2chan
+	if ok {
+		t.Errorf("bchan wasn't closed")
+	}
+}
+
 func buildTestChans(n int) []chan int {
 	s := make([]chan int, 0, n)
 	for i := 0; i < n; i++ {
