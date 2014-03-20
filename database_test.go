0ba8b4fe76128908d3761f5b297dc96f2f18ce22
Dustin Sallings <dustin@spy.net>
2014-03-19 23:29:24 -0700

    Test DB writer closing

--- database_test.go
+++ database_test.go
@@ -18,6 +18,18 @@ func TestKeyParsing(t *testing.T) {
 	}
 }
 
+func TestDBWClose(t *testing.T) {
+	w := dbWriter{"test", make(chan dbqitem), make(chan bool), nil}
+	err := w.Close()
+	if err != nil {
+		t.Errorf("First close expected success, got %v", err)
+	}
+	err = w.Close()
+	if err != errClosed {
+		t.Errorf("Expected second close to fail, got %v", err)
+	}
+}
+
 func BenchmarkKeyParsing(b *testing.B) {
 	input := "2012-08-26T20:46:01.911627314Z"
 
